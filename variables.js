fd2c7e0dbfd657936f95bb1a7624c49203963e61
carmenjoekalda <cjoekalda@gmail.com>
2023-09-12 19:38:55 +0300

    variables from  lesson 1

---
+++ variables.js
@@ -0,0 +1,22 @@
+// let and const
+let name = 'Kadi'
+console.log(name)
+const birthYear = '1999'
+// birthYear = '2000'
+
+// array
+const numbers =[1, 2, 3, 4, 5]
+console.log(numbers)
+// insert to the end
+numbers.push(0)
+console.log(numbers)
+// check by index (output = 3)
+console.log(numbers[2])
+
+// objects
+const person = {
+    firstName: 'Kadi',
+    lastName: 'Tamm',
+} 
+
+console.log(person.firstName)
