91eb659fe9dc7bb6045307659ec1f1c515429017
carmenjoekalda <cjoekalda@gmail.com>
2023-09-12 20:06:56 +0300

    math functions lesson2

---
+++ math.js
@@ -0,0 +1,36 @@
+// variables to work with
+const num1 = 100
+const num2 = 50
+let result
+
+// math operations, output = 150, 50, 2, 5000, 0
+result = num1 + num2
+result = num1 - num2
+result = num1 / num2
+result = num1 * num2
+result = num1 % num2
+// pi
+result = Math.PI
+// round, output = 3
+result = Math.round(2.5)
+// round up, output = 3
+result = Math.ceil(2.4)
+// round down, output = 2
+result = Math.floor(2.7)
+// square root, output = 4
+result = Math.sqrt(16)
+// absolute value, output = 5
+result = Math.abs(-5)
+// square, output = 64 = (4**3)
+result = Math.pow(4, 3)
+// random num, output = (0...1)
+result = Math.random()
+// random, output = (0...10]  
+result = Math.floor(Math.random() * 11)
+
+
+
+
+
+
+console.log(result)
