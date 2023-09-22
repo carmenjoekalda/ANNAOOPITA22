acd650e0fd0f138e1aa64aff6e50556ca3f2bda9
carmenjoekalda <cjoekalda@gmail.com>
2023-09-22 10:58:56 +0300

    idcode

--- idcode.js
+++ idcode.js
@@ -1,6 +1,6 @@
 // function 
 
-let id = '44501234215'
+let id = '34501234215'
 let gender
 
 function customerGender(id){
@@ -14,12 +14,12 @@ function customerGender(id){
             return gender
         } 
     } else {
-        return 'Unacceptable idcode.'
+        return 'Something went wrong'
     } 
 }
 
 function customerBirthyear(id){
-    if (customerGender != 'Unacceptable idcode.'){
+    if (customerGender != 'Something went wrong'){
         // returns the birthyear
         birthyear = id.slice(1, 3);
         return birthyear
@@ -29,7 +29,7 @@ function customerBirthyear(id){
 } 
 
 function customerBirthmonth(id){
-    if (customerGender != 'Unacceptable idcode.'){
+    if (customerGender != 'Something went wrong'){
         // returns the birthmonth
         birthmonth = id.slice(3, 5);
         return birthmonth
@@ -39,7 +39,7 @@ function customerBirthmonth(id){
 }
 
 function customerBirthday(id){
-    if (customerGender != 'Unacceptable idcode.'){
+    if (customerGender != 'Something went wrong'){
         // returns the birthday
         birthday = id.slice(5, 7);
         return birthday
@@ -49,17 +49,72 @@ function customerBirthday(id){
 } 
 
 function customerBirthplace(id){
-    if (customerGender != 'Unacceptable idcode.'){
+    if (customerGender != 'Something went wrong'){
         // returns the birthplace
-        birthplace = id.slice(7, 10);
-        return birthplace
+        birthplace = id.slice(7, 10); 
+        if (birthplace < 11){
+            return "Kuressaare haigla"
+        } 
+        else if (birthplace < 20){
+            return "Tartu Ülikooli Naistekliinik"
+        }
+        else if (birthplace < 151){
+            return "Ida-Tallinna keskhaigla or Pelgulinna sünnitusmaja"
+        } 
+        else if (birthplace < 161){
+            return "Keila haigla"
+        } 
+        else if (birthplace < 221){
+            return "Rapla haigla, Loksa haigla or Hiimunaa haigla (Kärdla)"
+        } 
+        else if (birthplace < 271){
+            return "Ida-Viru keskhaigla (Kohtla-Järve, endine Jõhvi)"
+        }
+        else if (birthplace < 371){
+            return "Maarjamõisa kliinikum (Tartu) or Jõgeva haigla"
+        } 
+        else if (birthplace < 421){
+            return "Narva haigla"
+        } 
+        else if (birthplace < 471){
+            return "Pärnu haigla"
+        } 
+        else if (birthplace < 491){
+            return "Haapsalu haigla"
+        } 
+        else if (birthplace < 520){
+            return "Järvamaa haigla (Paide)"
+        } 
+        else if (birthplace < 571){
+            return "Rakvere haigla or Tapa haigla"
+        } 
+        else if (birthplace < 601){
+            return "Valga haigla"
+        } 
+        else if (birthplace < 651){
+            return "Viljandi haigla"
+        } 
+        else if (birthplace < 701){
+            return "Lõuna-Eesti haigla (Võru) or Põlva haigla"
+        } 
     } else {
         return 'Something went wrong'
     } 
 } 
 
+function customerOrder(id){
+    if (customerGender != 'Something went wrong'){ 
+        // returns the order number
+    birthorder = id.slice(9, 10);
+        return birthorder
+    } else {
+        return 'Something went wrong'
+    }
+}
+
+
 function customerControlcode(id){
-    if (customerGender != 'Unacceptable idcode.'){
+    if (customerGender != 'Something went wrong'){
         // returns the idcode control code
         birthControlcode = id.slice(10, 11);
         return birthControlcode
@@ -69,9 +124,10 @@ function customerControlcode(id){
 } 
 
 console.log('The customer is a ' + (customerGender(id)) + '.')
-console.log("They were born in the year '" + (customerBirthyear(id)) + '')
-console.log(customerBirthmonth(id))
-console.log(customerBirthday(id))
-console.log(customerBirthplace(id))
-console.log(customerControlcode(id))
+console.log("They were born in the year '" + (customerBirthyear(id)) + '.')
+console.log('Their birthmonth is ' + (customerBirthmonth(id)) + '.')
+console.log('Their birthdate is ' + (customerBirthday(id)) + '.')
+console.log('They were born in ' + (customerBirthplace(id)) + '.')
+console.log('They were ' + (customerOrder(id)) + '. person from their gender to be born on that day')
+console.log('The control number of their id-code is ' + (customerControlcode(id)) + '.')
 
